013a2ded9c84ab39db6a5822aebb97e462e8a121
Gülnur <84268834+Gulnurph@users.noreply.github.com>
2022-12-03 22:28:00 +0300

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,18 @@
+﻿using SepetSistemi.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SepetSistemi.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(new ProductListViewModel());
+        }
+    }
+}
